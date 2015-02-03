005694bc1fefb334b4e7ec2ea4943eca247b6a0f
Justin Garrison <justinleegarrison@gmail.com>
2015-02-02 23:20:12 -0800

    removed startup script (not needed)

--- start-hts.sh
+++
@@ -1,8 +0,0 @@
-!#/bin/bash
-
-# Configure HDHomeRun
-# discover and config hdhomerun
-echo "["$(hdhomerun_config discover | cut -d ' ' -f 3)"]" | sudo tee /etc/dvbhdhomerun
-echo "tuner_type=ATSC" >> /etc/dvbhdhomerun
-
-/usr/bin/tvheadend -u hts -g video -C
